32a093d2533fecb0a9eb8b62d8a431a4d6e19390
Cerino Ligutom III <zeferinix@gmail.com>
2021-03-23 01:34:44 +0800

    chore: added express to api-todo

--- apps/api-todo/src/main.ts
+++ apps/api-todo/src/main.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import * as express from 'express';
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from api-todo</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`api-todo is listening on port ${PORT}...`);
+});
